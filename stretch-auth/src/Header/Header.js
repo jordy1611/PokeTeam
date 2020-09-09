fd20c658c8233cc4cd7196cc260b81a0d9fb16aa
Jordy Shryock <jordy1611@gmail.com>
2020-09-09 16:50:00 -0700

    Create initial header

--- stretch-auth/src/Header/Header.js
+++ stretch-auth/src/Header/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './Header.css'
 
-
-
+const Header = () => {
+  return (
+    <section className='header'>
+      <h1>THIS IS THE HEADER</h1>
+    </section>
+  )
+}
 
 export default Header
