252a2ef5efb02a0206d5c954125c638e71dda012
Jordy Shryock <jordy1611@gmail.com>
2020-09-09 16:55:25 -0700

    Create functional components for cardcontainer and cards

--- stretch-auth/src/PokeCard/PokeCard.js
+++ stretch-auth/src/PokeCard/PokeCard.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './PokeCard.css'
 
-
+const PokeCard = () => {
+  return (
+    <article className='poke-card'>
+      <p>PokeName</p>
+      <p>PokeType</p>
+      <p>PokePic</p>
+    </article>
+  )
+}
 
 
 
