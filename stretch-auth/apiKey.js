4588d6840eb745f8264c49e6c35512c05455a81f
Ben McClung <aurumvalian@gmail.com>
2020-09-10 11:42:11 -0600

    Implement partial firebase functionality with bugs

---
+++ stretch-auth/apiKey.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAgXadHK5sriJvfnzUufzuwyhXBEPExLAg",
+  authDomain: "turing-stretch-auth.firebaseapp.com",
+  databaseURL: "https://turing-stretch-auth.firebaseio.com",
+  projectId: "turing-stretch-auth",
+  storageBucket: "turing-stretch-auth.appspot.com",
+  messagingSenderId: "515130942385",
+  appId: "1:515130942385:web:082ea0c7e8c0371b985390",
+  measurementId: "G-4WMS8BCDBQ"
+}
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
