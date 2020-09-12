6f19023eadc70f0116bc2ba74f2138e356befe1a
Horacio Borrego <hborrego365@gmail.com>
2020-09-12 13:15:40 -0600

    Remove unnecessary code from Login

--- stretch-auth/src/Login/Login.js
+++ stretch-auth/src/Login/Login.js
@@ -5,39 +5,16 @@ import './Login.css'
 import { secrets } from '../secrets';
 import { firebaseui } from '../App/App';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-// import firebase from 'firebase';
 let firebase = require('firebase');
-console.log(secrets);
-
-// firebase.initializeApp(secrets);
-
-// const uiConfig = {
-//   signInFlow:'popup', 
-//   signInOptions: [
-//     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-//     firebase.auth.EmailAuthProvider.PROVIDER_ID
-//   ], 
-//   callbacks: {
-//     signInSuccess: () => false
-//   }
-// }
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // isSignedIn: false,
     }
     
   }
   
-  // componentDidMount = () => {
-  //   firebase.auth().onAuthStateChanged(user => {
-  //     this.setState({ isSignedIn:!!user })
-  //     console.log('user', user.displayName)
-  //   })
-  // }
-
   render() {
     return (
        <section>
@@ -47,7 +24,5 @@ class Login extends Component {
     )
   }
 }
-
-
 
 export default Login
