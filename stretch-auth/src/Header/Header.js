3581ee22d6878d4b69e42d731daff70bc78ca0ea
Horacio Borrego <hborrego365@gmail.com>
2020-09-14 10:52:57 -0600

    Add display welcome message in Header when a user is logged in

--- stretch-auth/src/Header/Header.js
+++ stretch-auth/src/Header/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import './Header.css'
 import pikaIcon from '../assets/pikaIcon.png'
 
 const Header = (props) => {
   return (
     <section className='header'>
+      {props.currentUserName ? <h2 className="user-message" >Welcome {props.currentUserName}</h2> : <Redirect to='/' />}
+      {props.currentUserName ? <h2 className="user-message" >Welcome {props.currentUserName}</h2> : <Redirect to='/' />}
       <Link to='/'><h1 className='page-name'>Pokemon Streeetch</h1></Link>
-     {props.currentUserName ? <Link to='/'><button className='header-button' onClick={props.logOutCurrentUser} className='header-button'>Log Out</button></Link> : <Link to='/login'><button className='header-button'>Login</button></Link>}
       <Link to='/'><div className='header-div'><img className='header-icon' src={pikaIcon} alt='pikachu icon'/><h1 className='page-name'>PokeTeam</h1></div></Link>
+     {props.currentUserName ? <Link to='/'><button className='header-button' onClick={props.logOutCurrentUser} className='header-button'>Log Out</button></Link> : <Link to='/login'><button className='header-button'>Login</button></Link>}
     </section>
   )
 }
