7dcb76110a9e55b142a5737a0200f3538b5435d0
Jordy Shryock <jordy1611@gmail.com>
2020-09-14 18:55:30 -0700

    Style poke cards

--- stretch-auth/src/PokeCard/PokeCard.js
+++ stretch-auth/src/PokeCard/PokeCard.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './PokeCard.css'
 import pokeBallIcon from '../assets/pokeball.png'
 const PokeCard = (props) => {
+  console.log(props.pokemon)
   return (
     <section>
     {(props.pokemon.pokemon &&
       <article className='poke-card'>
+      <img className="pokecard-pokeball"src={pokeBallIcon} />
+      <img className="pokecard-sprite" src={props.pokemon.sprite}/>
         <p className="pokecard-name">{props.pokemon.name}</p>
         <p className="pokecard-pokemon">{props.pokemon.pokemon}</p>
-        <p className="pokecard-id">{props.pokemon.id}</p>
-        <img className="pokecard-sprite" src={props.pokemon.sprite}/>
-        <img className="pokecard-pokeball"src={pokeBallIcon} />
+        <p className="pokecard-id">Id.{props.pokemon.id}</p>
+        <div className="pokecard-hp"><p className="pokecard-hp-text">HP:</p><div className="pokecard-hp-bar"></div></div>
         </article>) ||
       <article className='poke-card'>
         <img className="pokecard-pokeball"src={pokeBallIcon} />
